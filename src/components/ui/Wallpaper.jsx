d7e28b711276d6e154775d62a972cfef8487d533
Eduardo <eduardoguette@gmail.com>
2022-02-07 19:40:21 +0100

    fix html semantic

--- src/components/ui/Wallpaper.jsx
+++ src/components/ui/Wallpaper.jsx
@@ -1,18 +1,18 @@
 import React from 'react';
-import desktopNightImg from "/assets/images/bg-desktop-dark.jpg"
-import desktopDayImg from "/assets/images/bg-desktop-light.jpg"
+import desktopNightImg from '/assets/images/bg-desktop-dark.jpg';
+import desktopDayImg from '/assets/images/bg-desktop-light.jpg';
 
-import mobileDayImg from "/assets/images/bg-mobile-light.jpg"
-import mobileNightImg from "/assets/images/bg-mobile-dark.jpg"
+import mobileDayImg from '/assets/images/bg-mobile-light.jpg';
+import mobileNightImg from '/assets/images/bg-mobile-dark.jpg';
 
 export const Wallpaper = () => {
   return (
     <>
-      <picture className='night' role="Wallpaper nocturno">
+      <picture className='night' role='hero' aria-label='Wallpaper de noche'>
         <source media='(min-width: 640px)' srcSet={desktopNightImg} />
         <img src={mobileNightImg} alt='Hero image' />
       </picture>
-      <picture className='day' role="Wallpeper de día">
+      <picture className='day' role='hero' aria-label='Wallpaper de día'>
         <source media='(min-width: 640px)' srcSet={desktopDayImg} />
         <img src={mobileDayImg} alt='Hero image' />
       </picture>
